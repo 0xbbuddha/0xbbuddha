f42176e689f838306dfac32053bf3066893074dd
0xbbuddha <bouddha.whitehat@gmail.com>
2026-05-25 18:07:00 +0200

    update name

--- src/update_readme.py
+++ src/update_readme.py
@@ -2,7 +2,7 @@ import requests
 import asciichartpy as ac
 from datetime import date
 
-USERNAME = "PtitWaguri"
+USERNAME = "PtiteWaguri"
 HEADERS = {
     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36"
 }
